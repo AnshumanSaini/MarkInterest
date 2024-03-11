1fd8b377eb80be28461d42acc63a069f75ea5ab7
AnshumanSaini <90978443+AnshumanSaini@users.noreply.github.com>
2024-03-11 22:06:10 +0530

    .

--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import {Navbar} from '../components/Navbar'
+import {Feed} from '../components/Feed'
+import {PinDetail} from '../components/PinDetail'
+import {CreatePin} from '../components/CreatePin'
+import {Search} from '../components/Search'
+
 
 const Pins = () => {
+  const [searchTerm, setsearchTerm] = useState('')
   return (
     <div>Pins</div>
   )
