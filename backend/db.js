d58a20b1b0a1b373da9ea85484917ab61fab7af2
AnshumanSaini <90978443+AnshumanSaini@users.noreply.github.com>
2024-03-20 23:49:56 +0530

    .

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = ()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/MarkInterest", {useNewUrlParser: true}).then(()=> console.log("Connection is Successfull...")).catch((err)=> console.log(err));
+    mongoose.connect("mongodb+srv://rockasd25:W8ZPVfC2vF6B7mZg@cluster0.879lt6g.mongodb.net/MarkInterest", {useNewUrlParser: true}).then(()=> console.log("Connection is Successfull...")).catch((err)=> console.log(err));
     }
 
 module.exports = connectToMongo;
